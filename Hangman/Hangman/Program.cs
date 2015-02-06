7be5cc0494648ac078f27a882ac20fc87f0c32ad
PatrickSHYee <patrickshyee@gmail.com>
2015-02-06 14:33:09 -0700

    Programmed graphics and added some features.
    I need to make the removing body parts portion make more sense.

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -14,9 +14,20 @@ namespace Hangman
         // global variables
         static string name_Player = null;
         static string[] dictionary = { "gruesome", "bloosom", "processor", "bacon", "enlightenment" };
-        static string[] hangingMan = { "Head", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg", "Freebie" };
+        static string hangingMan = "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::-------:::::::::::::::::::::::::::::::::\n" +
+":::::::::::::::::::::::::::::::+:::o::::::::::::::::::::::::::::::::::\n" +
+":::::::::::::::::::::::::::::::+::/|\\::::::::::::::::::::::::::::::::\n" +
+":::::::::::::::::::::::::::::::+:::|::::::::::::::::::::::::::::::::::\n" +
+":::::::::::::::::::::::::::::::+::/:\\::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::";
         static Random randGen = new Random();
         static string WordToGuess = null;
+        static int turns = 0;
         public static string user_PlayingField { get; set; }  // this is going to be where the masked word stored
         static string wrongLetters = null;
         public static string letterGuessed { get; set; }
@@ -25,8 +36,7 @@ namespace Hangman
         {
             string GuessedLetter = null;
             bool quitter = true;
-            //bool guessing = true;
-            List<string> manHanging = new List<string>();  // an instance of the hanging man
+            string manHanging = new string(hangingMan.ToArray());  // an instance of the hanging man
 
             WordToGuess = dictionary[randGen.Next(0, dictionary.Length)];
 
@@ -35,18 +45,16 @@ namespace Hangman
             GetUserName();
             PrintScreen_instructions();
 
-            while (quitter)// || manHanging.Count >= 0)
+            while (quitter)
             {
-                Console.Clear();
-                //title screen
+                // title screen
                 PrintScreen_Title();
+                // Prints everything that user eyes
                 PrintGame(manHanging);
                 GuessedLetter = GetUserInput();
                 // see if the guessedLetter is in the wordToGuess
-                isInWord(GuessedLetter);
-
-                // Quit prompt
-                if (manHanging.Count == 0)
+                // the game is done
+                if (IsInWord(GuessedLetter, ref manHanging) || turns== 0)
                 {
                     quitter = printQuit();
                     if (quitter)
@@ -57,42 +65,62 @@ namespace Hangman
             }
         }
 
-        // this will set up or reset the game variables
-        static void initializer(ref List<string> man)
+        /// <summary>
+        /// this will set up or reset the game variables
+        /// </summary>
+        /// <param name="man">I want to be able to change the current hang man</param>
+        static void initializer(ref string man)
         {
+            string tempString = null;
+            turns = 7;
+            wrongLetters = null;
             Console.SetWindowSize(Console.WindowWidth, Console.LargestWindowHeight);
             WordToGuess = dictionary[randGen.Next(0, dictionary.Length)];
+            man = new string(hangingMan.ToArray());
 
             // setting up the blank underscored spaces
             for (int i = 0; i < WordToGuess.Length; i++)
             {
-                user_PlayingField += "_";
+                tempString += "_";
             }
 
-            // setting up the hanging man
-            if (man.Count != hangingMan.Length)
-            {
-                man = hangingMan.ToList<string>();
-            }
+            user_PlayingField = new string(tempString.ToArray());
         }
 
-        // if the user guesses the word return true and the game is won
-        // if the user enters in a number or some kind of sysmbol return false
-        // if the user puts a letter and search the wordToSearch, where ever the index of that letter is, copy the letter and place it at the
-        //   same index in 
-        static bool isInWord(string guess)
+        
+         
+        /// <summary>
+        /// if the user guesses the word return true and the game is won
+        /// if the user enters in a number or some kind of sysmbol return false
+        /// if the user puts a letter and search the wordToSearch, where ever the index of that letter is, copy the letter and place it at the
+        /// </summary>
+        /// <param name="guess">user input</param>
+        /// <param name="Deadman">ref to the hang man</param>
+        /// <returns></returns>
+        static bool IsInWord(string guess, ref string Deadman)
         {
-            int temp = 0;  // this does nothing for this function
-            string fieldHolder = null;
+            int part = 0;  // this does nothing for this function
+            char[] fieldHolder = user_PlayingField.ToArray();
+            string message_Wrong = "'##:::::'##:'########:::'#######::'##::: ##::'######:::\n" +
+" ##:'##: ##: ##.... ##:'##.... ##: ###:: ##:'##... ##::\n" +
+" ##: ##: ##: ##:::: ##: ##:::: ##: ####: ##: ##:::..:::\n" +
+" ##: ##: ##: ########:: ##:::: ##: ## ## ##: ##::'####:\n" +
+" ##: ##: ##: ##.. ##::: ##:::: ##: ##. ####: ##::: ##::\n" +
+" ##: ##: ##: ##::. ##:: ##:::: ##: ##:. ###: ##::: ##::\n" +
+". ###. ###:: ##:::. ##:. #######:: ##::. ##:. ######:::\n" +
+":...::...:::..:::::..:::.......:::..::::..:::......::::";
+            char[] newDeadman = Deadman.ToArray();
 
             // it's a number
-            if (int.TryParse(guess, out temp))
+            if (int.TryParse(guess, out part) )
             {
+                PrintASCII(message_Wrong);
                 return false;
             }
             // connect match
-            if (guess == WordToGuess)
+            if (guess == WordToGuess || user_PlayingField == WordToGuess)
             {
+                Console.WriteLine("You win!!");
                 return true;
             }
 
@@ -107,35 +135,83 @@ namespace Hangman
                         {
                             if (WordToGuess[i] == guess[0])
                             {
-                                fieldHolder += guess[0];
-                            }
-                            else
-                            {
-                                fieldHolder += "_";
+                                fieldHolder[i] = guess[0];
                             }
                         }
-                        user_PlayingField = fieldHolder;
-                    }
-                    else
+                        user_PlayingField = new string(fieldHolder);
+                    } else 
                     {
+                        part = randGen.Next(1, turns);
                         wrongLetters += guess;
+                        // messed up logic but it makes sense
+                        // do something
+                        // take head if temp
+                        if (part == 1)
+                        {
+                            for (int i = 0; i < Deadman.Length; i++)
+                            {
+                                if (Deadman[i] == 'o')
+                                {
+                                    Deadman.ToArray()[i] = ':';
+                                }
+                            }
+                            part++;
+                        }
+                        // left leg and left arm
+                        if (part == 2 || part == 4)
+                        {
+                            for (int i = 0; i < Deadman.Length; i++)
+                            {
+                                if (Deadman[i] == '/')
+                                {
+                                    newDeadman[i] = ':';
+                                }
+                            }
+                            turns--;
+                        }
+                        // torso
+                        if (part == 3)
+                        {
+                            for (int i = 0; i < Deadman.Length; i++)
+                            {
+                                if (Deadman[i] == '/')
+                                {
+                                    newDeadman[i] = ':';
+                                }
+                            }
+                            turns--;
+                        }
+                        // right arm and right leg
+                        if (part == 5 || part == 6)
+                        {
+                            for (int i = 0; i < Deadman.Length; i++)
+                            {
+                                if (Deadman[i] == '\\')
+                                {
+                                    newDeadman[i] = ':';
+                                }
+                            }
+                            turns--;
+                        }
                     }
                 }
             }
+
+            Deadman = new string(newDeadman);
             return false;
         }
 
-        // print letter guessed and guesses left (got my playing field)
-        static void PrintGame(List<string> DeadMan)
+        /// <summary>
+        ///  print letter guessed and guesses left (got my playing field)
+        /// </summary>
+        /// <param name="DeadMan">ref the hang man</param>
+
+        static void PrintGame(string DeadMan)
         {
             // the dead man
-            Console.Write("{0} these are the parts left are\n", name_Player);
-            for (int i = 0; i < DeadMan.Count; i++)
-            {
-                Console.Write(DeadMan[i] + " ");
-                System.Threading.Thread.Sleep(SPEED);
-            }
+            PrintASCII(DeadMan);
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
             // the correct letters
             for (int i = 0; i < user_PlayingField.Length; i++)
             {
@@ -159,10 +235,14 @@ namespace Hangman
             Console.WriteLine();
         }
 
-        // user input and returning a string for that input (got my user's input)
+        /// <summary>
+        /// user input and returning a string for that input (got my user's input)
+        /// </summary>
+        /// <returns>The user's input</returns>
+ 
         static string GetUserInput()
         {
-            string message = name_Player + ", please enter a letter: ";
+            string message = name_Player + ", please enter a letter or word: ";
 
             for (int i = 0; i < message.Length; i++)
             {
@@ -172,7 +252,11 @@ namespace Hangman
             return Console.ReadLine();
         }
 
-        // prompts the user to quit (got quit screen)
+        /// <summary>
+        /// prompts the user to quit (got quit screen)
+        /// </summary>
+        /// <returns>quit or not</returns>
+ 
         static bool printQuit()
         {
             Console.Write("Quitter? (yes or no) ");
@@ -185,7 +269,9 @@ namespace Hangman
             return true;
         }
         
-        // ask for the user's name and store it in name_Player
+        /// <summary>
+        /// ask for the user's name and store it in name_Player
+        /// </summary>
         static void GetUserName()
         {
             string message = "HI, HOW ARE YOU DOING? WHAT'S YOUR NAME? ";
@@ -195,37 +281,106 @@ namespace Hangman
                 System.Threading.Thread.Sleep(SPEED);
             }
             name_Player = Console.ReadLine();
+            if (name_Player == "")
+            {
+                name_Player = "You";
+            }
         }
 
 
-        // print title screen
+        /// <summary>
+        /// print title screen
+        /// </summary>
         static void PrintScreen_Title()
         {
-            string title = "THE HANGING MAN";
+            //string title = "THE HANGING MAN";
+            string niceTilte =
+"'##::::'##::::'###::::'##::: ##::'######:::'####:'##::: ##::'######:::\n" +
+" ##:::: ##:::'## ##::: ###:: ##:'##... ##::. ##:: ###:: ##:'##... ##::\n" +
+" ##:::: ##::'##:. ##:: ####: ##: ##:::..:::: ##:: ####: ##: ##:::..:::\n" +
+" #########:'##:::. ##: ## ## ##: ##::'####:: ##:: ## ## ##: ##::'####:\n" +
+" ##.... ##: #########: ##. ####: ##::: ##::: ##:: ##. ####: ##::: ##::\n" +
+" ##:::: ##: ##.... ##: ##:. ###: ##::: ##::: ##:: ##:. ###: ##::: ##::\n" +
+" ##:::: ##: ##:::: ##: ##::. ##:. ######:::'####: ##::. ##:. ######:::\n" +
+"..:::::..::..:::::..::..::::..:::......::::....::..::::..:::......::::\n";
 
-            for (int i = 0; i < title.Length; i++)
-            {
-                Console.Write(title[i]);
-                System.Threading.Thread.Sleep(SPEED);
-            }
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("THE");
+            PrintASCII(niceTilte);
             Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(Console.WindowWidth - 12, 9);
+            Console.WriteLine("MAN");
         }
 
-        // print instructions
+        /// <summary>
+        /// Fill in a solid space in the string
+        /// </summary>
+        /// <param name="color"></param>
+        static void FillInColor(ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+            Console.ForegroundColor = color;
+        }
+
+        /// <summary>
+        /// Prints the ASCII characters
+        /// </summary>
+        /// <param name="text"></param>
+        static void PrintASCII(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                // if ' ' shadow
+                if (text[i] == ' ')
+                {
+                    FillInColor(ConsoleColor.DarkGray);
+                }
+                // if  ' transistion color to the shadow color
+                if (text[i] == '\'' || text[i] == '.')
+                {
+                    FillInColor(ConsoleColor.Gray);
+                }
+                // if # color it in
+                if (text[i] == '#')
+                {
+                    FillInColor(ConsoleColor.DarkRed);
+                }
+                // if : color in with the background color
+                if (text[i] == ':')
+                {
+                    FillInColor(ConsoleColor.DarkBlue);
+                }
+                if (text[i] == '-' || text[i] == '+')
+                {
+                    FillInColor(ConsoleColor.DarkYellow);
+                }
+                // this is the color for the hanging man
+                if (text[i] == '|' || text[i] == 'o' || text[i] == '/'|| text[i] =='\\')
+                {
+                    FillInColor(ConsoleColor.DarkBlue);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.Write(text[i]);
+            }
+        }
+
+        /// <summary>
+        /// print instructions
+        /// </summary>
         static void PrintScreen_instructions()
         {
-            string instructions = "This version of Hangman, we are going to take our hanging man off his death pole.  " + name_Player.ToUpper() +"'s task is to guess letters individually or guess the whole word. This will grant the man a safe way off his death pole. If you mess up and guess wrong, I'll take off one of the body parts off.  If I decide to take the head, it means you loose the game.";
-            string instructions2 = "To Play:\n1) I'll think of a word.\n2) " + name_Player.ToUpper() + " will guess a letter\n3) If you guess correctly, I'll give you that letter to help you figure out the word";
+            string instructions = "To Play:\n1) I'll think of a word.\n2) " + name_Player.ToUpper() + 
+                " will guess a letter\n3) If "+ name_Player.ToUpper() + " guess correctly, I'll give "+ name_Player +
+                "\nthat letter to help you figure out the word\nIf " + name_Player + " guess wrong, I'll take a body part";
 
-            for (int i = 0; i < instructions.Length; i++)
-            {
-                Console.Write(instructions[i]);
-                //System.Threading.Thread.Sleep(50);
-            }
             Console.WriteLine();
-            for (int i = 0; i < instructions2.Length; i++){
-                Console.Write(instructions2[i]);
-                //System.Threading.Thread.Sleep(50);
+            for (int i = 0; i < instructions.Length; i++){
+                Console.Write(instructions[i]);
+                System.Threading.Thread.Sleep(50);
             }
             Console.WriteLine();
             System.Threading.Thread.Sleep(3000);
